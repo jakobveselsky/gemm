8a6b83c91ccc1e7cfc2464dfa5b072fb9d1e3cb6
jakobveselsky <jveselsky@luc.edu>
2022-07-07 14:08:37 -0500

    for pi

---
+++ data.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+neon = pd.read_csv('data.csv', sep=', ')
+serial = pd.read_csv('serial.csv', sep=', ')
+
+print("serial")
+print(serial)
+print("neon")
+print(neon)
+
+plt.plot(neon["N"],neon["time"], label='neon')
+plt.plot(serial["N"],serial["time"], label='serial')
+plt.title('serial vs neon')
+plt.xlabel('N')
+plt.ylabel('time')
+plt.grid(True)
+plt.legend()
+plt.show()
